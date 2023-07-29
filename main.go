7fc210e49946144b5df81de9b00331f88a4e67fe
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-29 20:54:37 +0330

    add new server in main file and route for test

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/pooulad/go-http-server/pkg/server"
+)
+
+func main(){
+	server := server.NewHttpServer("localhost",9090)
+	server.HandleFunc("/hello",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	server.Start()
+}
