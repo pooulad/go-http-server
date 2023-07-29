9c6d283e2cafd81584b14ce1e8147ad7e3bf7532
pooulad <86445458+pooulad@users.noreply.github.com>
2023-07-29 22:35:03 +0330

    move main file data here

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,14 @@
-package server
+package main
+
+import (
+	"net/http"
+	"github.com/pooulad/go-http-server/pkg/server"
+)
+
+func main(){
+	server := server.NewHttpServer("localhost",9090)
+	server.HandleFunc("/hello",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	server.Start()
+}
