cb8cdb3f796ba46ae406347585595f6c4f96d3c7
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-01 00:00:28 +0330

    add new variables for postgres info

--- env.sh
+++ env.sh
@@ -1,2 +1,10 @@
+# Server Env
 export SERVERHOST="localhost"
 export SERVERPORT=8080
+
+# Postgres
+export PGUSER="postgres"
+export PGPASSWORD="manamirmahdihastam"
+export PGHOST="localhost"
+export PGPORT=5432
+export PGDATABASE="GoHttpServer"
