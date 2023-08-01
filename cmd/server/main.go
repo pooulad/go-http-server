98b218023d11e97eef89cb709e6880d77faad377
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-01 12:52:50 +0330

    start db and migrate sql files in cmd server file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
 	"net/http"
+
+	"github.com/pooulad/go-http-server/internal/db"
+	"github.com/pooulad/go-http-server/internal/db/postgres"
 	"github.com/pooulad/go-http-server/pkg/config"
 	"github.com/pooulad/go-http-server/pkg/server"
 )
 
-func main(){
+func main() {
 	cnf := config.LoadConfigOrPanic()
+	pg,err := postgres.NewPostgres(cnf.Postgres)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.MigrateOrPanic(context.Background(),pg);err != nil {
+		panic(err)
+	}
 	server := server.NewHttpServer(cnf.Server)
-	server.HandleFunc("/hello",func(w http.ResponseWriter, r *http.Request) {
+	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world!"))
 	})
 	server.Start()
