c5054138ab6e1b6e152fc05c3dd515649b205566
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 20:30:48 +0330

    Update track.go

--- api/track.go
+++ api/track.go
@@ -2,7 +2,7 @@ package api
 
 type Track struct {
 	Id     *int64  `db:"track_id" json:"id"`
-	Name   *string `db:"track_name"json:"name"`
-	Artist *string `db:"track_artist"json:"artist"`
-	Length *int    `db:"track_length"json:"length"`
+	Name   *string `db:"track_name" json:"name"`
+	Artist *string `db:"track_artist" json:"artist"`
+	Length *int    `db:"track_length" json:"length"`
 }
