3c67da97366998d9750486b06945b132493a5900
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 20:56:04 +0330

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,6 +5,6 @@ export SERVERPORT=8080
 # Postgres
 export PGUSER="postgres"
 export PGPASSWORD="postgres"
-export PGHOST="localhost"
+export PGHOST="0.0.0.0"
 export PGPORT=5432
 export PGDATABASE="GoHttpServer"
