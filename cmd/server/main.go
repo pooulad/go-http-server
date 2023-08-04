571361af8224b96f99eeedb5e9b88d2178b5b155
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 19:42:43 +0330

    add handler in cms server file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"context"
-	"net/http"
-
 	"github.com/pooulad/go-http-server/internal/db"
 	"github.com/pooulad/go-http-server/internal/db/postgres"
+	"github.com/pooulad/go-http-server/internal/handler"
+	"github.com/pooulad/go-http-server/internal/repo"
 	"github.com/pooulad/go-http-server/pkg/config"
 	"github.com/pooulad/go-http-server/pkg/server"
 )
 
 func main() {
 	cnf := config.LoadConfigOrPanic()
-	pg,err := postgres.NewPostgres(cnf.Postgres)
+	pg, err := postgres.NewPostgres(cnf.Postgres)
 	if err != nil {
 		panic(err)
 	}
-	if err := db.MigrateOrPanic(context.Background(),pg);err != nil {
+	if err := db.MigrateOrPanic(context.Background(), pg); err != nil {
 		panic(err)
 	}
+
+	dbRepo := repo.NewTrackRepo(pg)
+	trackHandler := handler.NewTrackHandler(dbRepo)
 	server := server.NewHttpServer(cnf.Server)
-	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world!"))
-	})
+	server.HandleFunc("/track", trackHandler.Handle)
 	server.Start()
 }
